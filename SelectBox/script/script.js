5c78176cec38a00cb4ec7dc278eac585b22cc060
Yusufhan-Erbas <63731504+Yusufhan-Erbas@users.noreply.github.com>
2022-12-14 15:11:23 +0300

    Add files via upload

---
+++ SelectBox/script/script.js
@@ -0,0 +1,13 @@
+
+var ul=document.createElement("ul");
+var div=document.getElementById("main");
+div.appendChild(ul);
+ul.querySelector(".menu");
+ul.setAttribute("id","menu")
+var len=ul.style.length;
+var li=document.createElement("li");
+ul.appendChild(li);
+
+
+
+
